d3dac3500168858a3cac26de2834bf67ab80bf60
Laurent Binder <laurentbinder@gmail.com>
2020-02-11 14:32:31 +0100

    TasksController def index, def show, def new, def create

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,14 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   # CREATE
-
-
+  get 'tasks/new', to: 'tasks#new'
+  post 'tasks', to: 'tasks#create'
   # READ
   # read all tasks
+  get 'tasks', to: 'tasks#index'
 
   # read a task
+  get 'tasks/:id', to: 'tasks#show', as: :task
 
   # UPDATE
 
