6129a0c2f01a8fde9cb5818ea3f8e8cf13d38c6e
Laurent Binder <laurentbinder@gmail.com>
2020-02-11 11:55:14 +0100

    add migrate

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # CREATE
+
+
+  # READ
+  # read all tasks
+
+  # read a task
+
+  # UPDATE
+
+
+  # DELETE
 end
