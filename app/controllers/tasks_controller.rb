96ecd6c0b17215c4c365f01caa2d8913c87ab21f
Laurent Binder <laurentbinder@gmail.com>
2020-10-05 16:58:44 +0200

    add index show and new DEF in TasksController

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,19 @@
 class TasksController < ApplicationController
+  before_action
+
+  # read all tasks
+  def index
+    @tasks = Task.all
+  end
+
+  # read one task
+  def show
+    @task = Task.find(params[:id])
+  end
+
+  def new
+    task = Task.new
+    task.save
+  end
+
 end
