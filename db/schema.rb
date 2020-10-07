5790005ca0af9ee465170de7e86667714cdf1e4c
Laurent Binder <laurentbinder@gmail.com>
2020-10-07 13:00:51 +0200

    db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_07_104638) do
+ActiveRecord::Schema.define(version: 2020_10_05_140022) do
 
   create_table "tasks", force: :cascade do |t|
     t.string "title"
